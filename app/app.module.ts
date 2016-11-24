4b84ba7068345f5419bfba0971bbe72657df23a5
Paralaxe <primataxdeus@gmail.com>
2016-11-23 22:40:31 -0200

    First commit with basic angular 2 from scratch, with components, routing

---
+++ app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HomepageComponent } from './homepage/homepage.component';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+	imports: [
+		BrowserModule,
+        AppRoutingModule
+	],
+	declarations: [
+		AppComponent,
+		HomepageComponent
+	],
+	bootstrap: [
+		AppComponent
+	]
+})
+export class AppModule {}
---
+++ app/homepage/homepage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'homepage',
+	templateUrl: 'homepage.component.html'
+})
+export class HomepageComponent {}
