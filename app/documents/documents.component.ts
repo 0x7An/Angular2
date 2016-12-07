b3bfea43a83b742a587d3eb8d018f0541cca60f7
Paralaxe <primataxdeus@gmail.com>
2016-12-07 12:33:58 -0200

    Adding Bootstrap

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -13,21 +13,21 @@ export class DocumentsComponent {
 			description: 'asdfasdfasdf asdfasd',
 			file_url: 'http://google.com',
 			updated_at: '11/11/16',
-			image_url: 'http://google.com',
+			image_url: 'https://upload.wikimedia.org/wikipedia/commons/b/bd/Yu_Yu_Hakusho_logo.png',
 		},
 		{
 			title: "My Second Doc",
 			description: 'asdfasdfasdf asdfasd',
 			file_url: 'http://google.com',
 			updated_at: '11/11/16',
-			image_url: 'http://google.com',
+			image_url: 'https://static.pexels.com/photos/34123/pexels-photo.jpg',
 		},
 		{
 			title: "My Last Doc",
 			description: 'asdfasdfasdf asdfasd',
 			file_url: 'http://google.com',
 			updated_at: '11/11/16',
-			image_url: 'http://google.com',
+			image_url: 'http://i.imgur.com/PRg3IbO.jpg',
 		}
 	]
 }
